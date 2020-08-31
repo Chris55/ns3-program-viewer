2b8381cafe4ce25c54de7dc2a2b40e88e5c598a8
Chris55 <christian.florentz@gmail.com>
2020-08-31 19:56:14 +0200

    added doc struct

--- server/ns3/organ.js
+++ server/ns3/organ.js
@@ -107,9 +107,7 @@ exports.getOrgan = (buffer, panelOffset, splitEnabled) => {
         /**
          * Offset in file:
          *
-         * @example
-         *
-         * Volume:
+         * `Volume:
          * 0xB6 (b2-b0), 0xB7 (b7-b4): 7-bit = 0/127 range
          *
          * Morph Wheel:
