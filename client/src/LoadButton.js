ffbe943fb56f7ce0133f7b08485988216d4f3787
Chris55 <christian.florentz@gmail.com>
2021-10-02 20:33:18 +0200

    cleanup

--- client/src/LoadButton.js
+++ client/src/LoadButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 import FileUploaderButton from "./utils/file-uploader-button";
-import {useDispatch, useSelector} from "react-redux";
-import {allSupportedTypes, loadFiles, nordSelector} from "./features/nord/nordSliceReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { allSupportedTypes, loadFiles, nordSelector } from "./features/nord/nordSliceReducer";
 
 const LoadButton = (props) => {
     const dispatch = useDispatch();
-    const {loading} = useSelector(nordSelector);
+    const { loading } = useSelector(nordSelector);
 
     const handleFiles = (files) => {
         if (files) {
