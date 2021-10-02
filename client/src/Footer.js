ac11a1f83d7e1ef357f499aa9c303bee58aeaaf7
Chris55 <christian.florentz@gmail.com>
2021-10-02 16:44:17 +0200

    fixed position

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,13 +5,13 @@ import AppName from "./AppName";
 const Footer = () => {
     return (
         <Row>
-            <Col className="flex-grow-1 text-muted text-right mx-sm-5 mx-sx-2">
-                <hr />
-                <div className="">
-                    <small>
+            <Col className="flex-grow-1 text-muted text-right mx-sm-5 mx-sx-2 border-0">
+                <hr className="footer-line"/>
+
+                    <div style={{fontSize: "0.9rem"}}>
                         <AppName /> (c) {new Date().getFullYear()} Christian Florentz
-                    </small>
-                </div>
+                    </div>
+
             </Col>
         </Row>
     );
