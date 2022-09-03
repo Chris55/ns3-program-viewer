a440fc9388513827dbf0f2dc5fe8f1ff1dfbf65f
Chris55 <christian.florentz@gmail.com>
2022-09-03 16:41:13 +0200

    added emitter to bundle all backend

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,10 @@ module.exports = [
         entry: "./src/server/server.js",
         output: {
             path: path.resolve(__dirname, "build"),
-            filename: "server.min.js"
+            filename: "server.min.js",
         },
         target: "node",
         externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
-        externals: [nodeExternals()] // in order to ignore all modules in node_modules folder
-    }
+        //externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    },
 ];
