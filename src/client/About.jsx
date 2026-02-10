eec3b0847b8acdff57344b431d355ad31a39acd1
Chris55 <christian.florentz@gmail.com>
2026-02-10 08:32:17 +0100

    updated env

--- src/client/About.jsx
+++ src/client/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Navbar, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import AppName from "./AppName";
 import Footer from "./Footer";
 import { openUrl } from "./utils/handy";
